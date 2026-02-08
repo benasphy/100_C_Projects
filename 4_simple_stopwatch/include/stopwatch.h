343ef82be5c1d2c3f6e9c5e68bbb23e9f5efb351
benasphy <binidani1903@gmail.com>
2026-02-08 16:30:59 +0300

    stopwatch header file

---
+++ 4_simple_stopwatch/include/stopwatch.h
@@ -0,0 +1,10 @@
+#ifndef STOPWATCH_H
+#define STOPWATCH_H
+
+void show_menu(void);
+void start_stopwatch(void);
+void stop_stopwatch(void);
+void reset_stopwatch(void);
+void show_elapsed_time(void);
+
+#endif
