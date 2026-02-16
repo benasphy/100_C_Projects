e317223a1536a75ba709fcff54140fe7eab64703
benasphy <binidani1903@gmail.com>
2026-02-16 11:19:08 +0300

    converter header file

---
+++ 9_unit_converter/include/converter.h
@@ -0,0 +1,17 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+
+//length
+double metersToKilometers(double meters);
+double kilometersToMeters(double km);
+
+//weight
+double kilogramToPounds(double kg);
+double poundsToKilogram(double pounds);
+
+//Temperature
+double celsiusToFahrenheit(double c);
+double fahrenheitToCelsius(double f);
+
+
+#endif
