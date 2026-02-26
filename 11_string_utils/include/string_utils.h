6ed265762e54003d4393a985d855b32871fd17c1
benasphy <binidani1903@gmail.com>
2026-02-26 20:01:09 +0300

    header file

---
+++ 11_string_utils/include/string_utils.h
@@ -0,0 +1,8 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+int my_strlen(const char *str);
+void my_strcpy(char *dest, char *src);
+int my_strcmp(const char *str1, const char *str2);
+
+#endif
